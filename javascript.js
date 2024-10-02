3ec91bcfbd74ebc6940e4d816c0600b81f0090f6
Richard Paynter <elfsmelf@gmail.com>
2024-10-02 22:13:14 +1000

    Added styling and Javascript DOM stuff for ui

--- javascript.js
+++ javascript.js
@@ -1,5 +1,47 @@
+// get DOM elements:
+
+const paperButtonElement = document.querySelector(".js-paper-button");
+const scissorsButtonElement = document.querySelector(".js-scissors-button");
+const rockButtonElement = document.querySelector(".js-rock-button");
+const resultElement = document.querySelector(".js-result");
+const roundElement = document.querySelector(".js-round");
+const roundDescriptionElement = document.querySelector(".js-round-title");
+const gameButtonElements = document.querySelector(".player-move-buttons");
+const selectMoveElement = document.querySelector(".js-select-move");
+const titleElement = document.querySelector(".js-title");
+const gameOverExistingElement = document.querySelector(".js-game-over-element");
+const roundCounterSpanElement = document.querySelector(".js-round-counter");
+const playerScoreElement = document.querySelector(".js-player-scores");
+const roundVictorElement = document.querySelector(".js-round-victor");
+const computerMoveElement = document.querySelector(".js-computer-move-element");
+const humanMoveElement = document.querySelector(".js-human-move-element");
+
+const gameOverElement = document.createElement("div");
+const gameOverContent = document.createElement("h2");
+
+const gameOverMessageElement = document.createElement("p");
+
+gameOverContent.textContent = "Game Over!";
+const restartGameButton = document.createElement("button");
+restartGameButton.textContent = "Restart";
+restartGameButton.classList.add("js-restart-button");
+
+gameOverElement.appendChild(gameOverContent);
+gameOverElement.appendChild(gameOverMessageElement);
+gameOverElement.appendChild(restartGameButton);
+
+restartGameButton.addEventListener("click", restartButtonClick);
+
+paperButtonElement.addEventListener("click", () => getHumanChoice("paper"));
+scissorsButtonElement.addEventListener("click", () =>
+  getHumanChoice("scissors")
+);
+rockButtonElement.addEventListener("click", () => getHumanChoice("rock"));
+
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
+let roundStatus = "";
 
 function getComputerChoice() {
   randomNumber = Math.random();
@@ -12,37 +54,48 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt("What is your choice?").toLowerCase();
-  console.log(`Human Choice = ${humanChoice}`);
-  return humanChoice;
+function getHumanChoice(choice) {
+  console.log(`${choice}`);
+  playerAction(choice);
+  return choice;
 }
 
 function playRound(humanChoice, computerChoice) {
+  const roundData = {};
+
   if (humanChoice === "rock" && computerChoice === "rock") {
-    return `CASE 1: You played Rock & the computer played Rock | It's a DRAW!`;
+    roundStatus = "Draw";
+    return `You played Rock & the computer played Rock`;
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
     humanScore++;
-    return `CASE 2: You played Rock & the computer played Scissors | You WIN!`;
+    roundStatus = "Win";
+    return `You played Rock & the computer played Scissors`;
   } else if (humanChoice === "rock" && computerChoice === "paper") {
     computerScore++;
-    return `CASE 3: You played Rock & the computer played Paper | You LOSE!`;
+    roundStatus = "Loss";
+    return `You played Rock & the computer played Paper`;
   } else if (humanChoice === "paper" && computerChoice === "rock") {
     humanScore++;
-    return `CASE 4: You played Paper & the computer played Rock | You WIN!`;
+    roundStatus = "Win";
+    return `You played Paper & the computer played Rock`;
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
     computerScore++;
-    return `CASE 5: You played Paper & the computer played Scissors | You LOSE!`;
+    roundStatus = "Loss";
+    return `You played Paper & the computer played Scissors`;
   } else if (humanChoice === "paper" && computerChoice === "paper") {
-    return `CASE 6: You played Paper & the computer played Paper | It's a DRAW`;
+    roundStatus = "Draw";
+    return `You played Paper & the computer played Paper`;
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
     computerScore++;
-    return `CASE 7: You played Scissors & the computer played Rock | You LOSE`;
+    roundStatus = "Draw";
+    return `You played Scissors & the computer played Rock`;
   } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-    return `CASE 8: you played Scissors & the computer played Scissors | It's a DRAW`;
+    roundStatus = "Draw";
+    return `You played Scissors & the computer played Scissors`;
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
     humanScore++;
-    return `CASE 9: You played Scissors && the computer played Paper | You WIN!`;
+    roundStatus = "Win";
+    return `You played Scissors && the computer played Paper`;
   } else {
     return "Something weird happened";
   }
@@ -58,17 +111,62 @@ function checkWinner(humanScore, computerScore) {
   }
 }
 
-function playGame() {
-  for (let round = 1; round <= 5; round++) {
-    console.log(`Round ${round}`);
-    const computerChoice = getComputerChoice();
-    const humanChoice = getHumanChoice();
-    console.log(playRound(humanChoice, computerChoice));
-    console.log(
-      `Current score - Human: ${humanScore}, Computer: ${computerScore}\n`
-    );
+function roundCounter(humanScore, computerScore) {
+  console.log(round);
+  if (round < 5) {
+  } else {
+    console.log(checkWinner(humanScore, computerScore));
+    gameButtonElements.remove();
+    selectMoveElement.remove();
+    gameOverExistingElement.appendChild(gameOverElement);
+    gameOverMessageElement.textContent = checkWinner(humanScore, computerScore);
   }
-  console.log(checkWinner(humanScore, computerScore));
 }
 
-playGame();
+function playerAction(humanChoice) {
+  const computerChoice = getComputerChoice();
+
+  console.log(`${computerChoice} and ${humanChoice}`);
+  const result = playRound(humanChoice, computerChoice);
+
+  if (roundStatus === "Win") {
+    roundVictorElement.textContent = "🧑 WINS!";
+  } else if (roundStatus === "Draw") {
+    roundVictorElement.textContent = "It's A DRAW!";
+  } else {
+    roundVictorElement.textContent = "🤖 WINS!";
+  }
+
+  if (computerChoice === "rock") {
+    computerMoveElement.textContent = "🪨";
+  } else if (computerChoice === "paper") {
+    computerMoveElement.textContent = "📄";
+  } else {
+    computerMoveElement.textContent = "✂️";
+  }
+
+  if (humanChoice === "rock") {
+    humanMoveElement.textContent = "🪨";
+  } else if (humanChoice === "paper") {
+    humanMoveElement.textContent = "📄";
+  } else {
+    humanMoveElement.textContent = "✂️";
+  }
+
+  console.log(`Human Score: ${humanScore} - Computer Score ${computerScore}`);
+  playerScoreElement.textContent = `🤖 ${computerScore} 🧑 ${humanScore}`;
+
+  // Create a new div element for each result
+  roundDescriptionElement.textContent = `Round ${round + 1}`;
+  resultElement.textContent = result;
+
+  // Update the round counter
+  round++;
+  roundCounterSpanElement.textContent = round;
+  roundCounter(humanScore, computerScore);
+}
+
+function restartButtonClick() {
+  console.log("restart button clicked");
+  location.reload();
+}
